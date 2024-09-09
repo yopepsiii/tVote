211ea46ce3a4d084a269e2888f1e886ff0ab8854
v1nn1ty <v1nn1ty.add@gmail.com>
2024-09-09 21:38:29 +0300

    пофиксил кое-какие штуки

--- app/config.py
+++ app/config.py
@@ -7,6 +7,8 @@ class Settings(BaseSettings):
     database_name: str
     database_username: str
     database_password: str
+    redis_host: str
+    redis_port: int
     owner_email: str
     secret_key: str
     algorithm: str
